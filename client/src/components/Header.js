5808972fb400cdcdb0fa1be404739fd077925dc7
Ardie C. Derrayal <acderrayal@gmail.com>
2020-02-27 13:58:42 +0800

    third commit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,6 @@ import Typography from '@material-ui/core/Typography';
 
 export const Header = () => {
     return (
-        <Typography variant="h4" align="center" gutterBottom>Mern Expense Tracker</Typography>
+        <Typography variant="h4" align="center" gutterBottom>Mern Stack Expense Tracker</Typography>
     )
 }
